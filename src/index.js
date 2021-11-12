c24c3081ff77f0923629d7034970f849ff881f01
Marlena Tsang <Marlena345@gmail.com>
2021-11-12 15:04:03 -0500

    header and form styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hi')
+//load page with nothing. just drop down menu
+
+//event listener 1: when form submits(dropdown menu), fetch the option submitted
+
+//display the clubs that correlates to option value (iteriates)
+
+//event listener 2: hidden feat of info about club 
+
+//event listener 3: like button w display of likes next to it
