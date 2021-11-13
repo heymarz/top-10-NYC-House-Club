dcaac2f95fd5e782ce9e7b27e5b1f2d7d6dcb568
Marlena Tsang <Marlena345@gmail.com>
2021-11-13 12:04:26 -0500

    images on page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-//load page with nothing. just drop down menu
+let venueArray=[];
 
+//load page with nothing. just drop down menu
+let eHandler = function(){
+  fetch('http://localhost:3000/venues')
+  .then(resp => resp.json())
+  .then(data=>{renderOnPage(data) });
+}
+
+function renderOnPage(venues){
+  //select contatiner where element will go
+  let infoContainer = document.querySelector("#infoContainer");
+
+  venues.forEach(venue => {
+  //create an html element that the innertext of the name
+  const h3 = document.createElement("h3");
+  h3.innerText =venue.name;
+  const img = document.createElement("img");
+  img.src = venue.image;
+  img.width = 500;
+
+
+  //append the element to the container     
+  let div = document.createElement("div");
+  div.classList.add("sleeve");
+  div.append(h3, img);
+  infoContainer.append(div)    
+  }); 
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  eHandler();
+
+})
+
+
+/*
 //event listener 1: when form submits(dropdown menu), fetch the option submitted
 
-//display the clubs that correlates to option value (iteriates)
+//display the clubs that correlates to option value 
 
 //event listener 2: hidden feat of info about club 
 
 //event listener 3: like button w display of likes next to it
+
+*/
+
